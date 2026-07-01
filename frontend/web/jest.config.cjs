d8cf3ac2cdb69fce53b787e843996cdd75fd3d66
Casey Liew <cliew2058@gmail.com>
2026-07-01 02:05:37 +0100

    resolve conflict

--- frontend/web/jest.config.cjs
+++ frontend/web/jest.config.cjs
@@ -2,17 +2,6 @@ module.exports = {
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
   moduleNameMapper: {
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-  },
-  "moduleNameMapper": {
-  "^maplibre-gl$": "<rootDir>/src/__mocks__/maplibre-gl.js"
-}
-};
-
-export default {
-  testEnvironment: "jsdom",
-  setupFilesAfterFramework: ["./src/setupTests.js"],
-  moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "<rootDir>/src/__mocks__/fileMock.js",
     "^maplibre-gl$": "<rootDir>/src/__mocks__/maplibre-gl.js",
   },
